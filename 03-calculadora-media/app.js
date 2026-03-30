630c63056f0e8dea1575c298b0edbc86cc4d5697
aryely-lang <25203777@CFP127ALUNO.LOCAL>
2026-03-30 08:25:18 -0300

    atividade calculadora de media finalizada

--- 03-calculadora-media/app.js
+++ 03-calculadora-media/app.js
@@ -1,7 +1,16 @@
 'use strict'
 
+//função responsavel por remover as classes para não gerar erro
+//ao tentar realizar o calculo da media outra vez
+function removerClasses() {
+
+    //pega o conteiner resultado e remove a classe anterior
+    const resultado = document.getElementById('resultado')
+    resultado.classList.remove('aprovado', 'recuperacao', 'reprovado')
+}
+
 //função para receber as notas e calcular a média do aluno
-function calcularMedia(nota1, nota2, nota3){
+function calcularMedia(nota1, nota2, nota3) {
 
     //soma todos os valores das notas e divide por 3
     const calculoDaMedia = (nota1 + nota2 + nota3) / 3
@@ -9,7 +18,7 @@ function calcularMedia(nota1, nota2, nota3){
 }
 
 //função responsavel por exibir o resultado da soma
-function exibirResultado(){
+function exibirResultado() {
 
     //cria uma constante, recebe o parametro do html e tranforma para number
     //.value para pegar o valor e não o conteiner
@@ -19,24 +28,48 @@ function exibirResultado(){
 
     //constante que recebe o parametro "resultado" que vem do html pelo id
     const resultado = document.getElementById('resultado')
-    
-    //chama as funções passando os paramentros
-    resultado.textContent = calcularMedia(nota01, nota02, nota03)
+
+    //cria uma variavel "media" e passa os parametros nota1 2 e 3
+    const media = calcularMedia(nota01, nota02, nota03)
+    resultado.textContent = media
+
+    //chama apenas a função "trocar cor"
+    //passando o parametro "media" que guarda
+    //o conteiner "resultado"
+    trocarCor(media)
 
 }
+
 //função responsavel por trocar a cor de acordo com o resultado da media 
 function trocarCor(calculoDaMedia) {
-    const resultado = document.getElementById('resultado')
-    removerClasses()
 
-    if (calculoDaMedia >= 7 ){
-        //aprovado
+    //pega o elemento guardado no "resuldado" pelo id
+    const resultado = document.getElementById('resultado')
+
+    removerClasses() //chama a função para remover o resultado depois qu trocar a cor
+
+    //condicional que atribui o status das notas com base na media final
+    if (calculoDaMedia >= 7) {
+        //status aprovado
+        //se a média for suficiente, adiciona a classe do CSS "aprovado" para mudar a cor/estilo
+        //e atualiza o texto exibido na tela para "aprovado"
+        //ao inves de exibir o resultado do calulo da media
         resultado.classList.add('aprovado')
-    }else if (calculoDaMedia >= 5 && calculoDaMedia <= 7){
-        //recuperação
-        resultado.classList.add('recuperação')
-    }else if (calculoDaMedia < 5){
-        //reprovado
+        resultado.textContent = "aprovado"
+
+    } else if (calculoDaMedia >= 5 && calculoDaMedia < 7) {
+        //status recuperação
+        resultado.classList.add('recuperacao')
+        resultado.textContent = "recuperação"
+
+    } else if (calculoDaMedia < 5) {
+        //status reprovado
         resultado.classList.add('reprovado')
+        resultado.textContent = "reprovado"
     }
 }
+
+//pega o conteiner "calcular"
+//atribui para ele 1 click e chama o exibir resultado
+document.getElementById('calcular')
+    .addEventListener('click', exibirResultado)
